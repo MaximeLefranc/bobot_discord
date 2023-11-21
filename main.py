0b6593da8f0ab823eea85c47efd504fdbb2a8356
Maxime <maxilefranc@gmail.com>
2023-11-21 09:26:04 -0500

    added event "on_member_join"

--- main.py
+++ main.py
@@ -12,9 +12,16 @@ async def on_ready():
 
 
 @client.event
-async def on_message(message):
+async def on_message(message: discord.Message):
     if message.content.lower() == 'ping':
         await message.channel.send('pong', delete_after=5)
+
+
+@client.event
+async def on_member_join(member: discord.Member):
+    if isinstance(general_channel := client.get_channel(1176513829904056321), discord.TextChannel):
+        await general_channel.send(f'Bienvenue sur le serveur {member.display_name}!')
+
 
 if BOT_TOKEN:
     client.run(BOT_TOKEN)
